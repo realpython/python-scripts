f6a1a2688f5955fcec6f84bb5001435d421ae566
Michael Herman <hermanmu@gmail.com>
2014-05-02 16:52:52 -0500

    Create rename_with_slice.py

---
+++ rename_with_slice.py
@@ -0,0 +1,14 @@
+import os
+import glob
+
+os.chdir("/Users/mikeherman/repos/bugs/se-platform/se/core/permissions")
+for file in glob.glob("*.json"):
+    file_name = os.path.splitext(file)[0]
+    extension = os.path.splitext(file)[1]
+    new_file_name = file_name[:-6] + extension
+    try:
+        os.rename(file, new_file_name)
+    except OSError, e:
+        print e
+    else:
+        print "Renamed {} to {}".format(file, new_file_name)
