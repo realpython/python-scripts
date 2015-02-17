25e5b3a8d0a4cf75cbf48f5bf54ae2f18832b38a
Michael Herman <hermanmu@gmail.com>
2015-02-16 22:58:11 -0700

    basic sql2csv

---
+++ 24_sql2csv.py
@@ -0,0 +1,17 @@
+import sys
+import csv
+import sqlite3
+
+if len(sys.argv) < 3:
+    print "Use: {0} DATABASE_NAME TABLE_NAME".format(sys.argv[0])
+    exit()
+
+conn = sqlite3.connect(sys.argv[1])
+cur = conn.cursor()
+data = cur.execute("SELECT * FROM {0}".format(sys.argv[2]))
+
+with open('output.csv', 'wb') as f:
+    writer = csv.writer(f)
+    writer.writerows(data)
+
+conn.close()
