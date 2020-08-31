c6bd26c8872a160a484f02aaa6e2c60b6c46181a
yeswanth9171 <69500697+yeswanth9171@users.noreply.github.com>
2020-08-31 15:01:25 +0530

    Create password_generator.py

---
+++ scripts/password_generator.py
@@ -0,0 +1,42 @@
+import random
+import string
+Green = "\033[32m"
+Blue = "\034[40m"
+End = "\033[0m"
+Red = "\033[31m"
+specialChars = "!@#$%^&*-_+="
+all = string.ascii_letters + string.digits + specialChars
+
+
+
+def passowrd_generator(number):
+    password = []
+    for i in range(number):
+        for j in random.choice(all):
+            password.append(j)
+    return "".join(x for x in password)
+
+def validate_password(result):
+    Upper = 0
+    Lower = 0
+    digits = 0
+    special_characters = 0
+    for i in result:
+        if i.isupper() == True:
+            Upper += 1     
+        elif i.islower() == True:
+            Lower += 1
+        elif i in specialChars:
+            special_characters += 1
+        elif i in all:
+            digits += 1
+    if Upper and Lower and special_characters and digits >=1:
+        print(Green + "{}    passowrd is good ".format(result) +End)        
+    else:
+        print(Red+ "{}    is not a good passowrd. It does not contain all the requirments. Rerun the script again".format(result) + End)
+
+if __name__ == "__main__":
+        number = int(raw_input("enter the number : "))
+        result = passowrd_generator(number)
+        validate_password(result)
+    
