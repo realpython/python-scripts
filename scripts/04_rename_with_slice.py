2f58d9696880cc4fa1901994b7a46b1d7fd8986c
Dmytro Khomiakov <dkhomia@softserveinc.com>
2023-04-14 12:57:05 +0300

    Remastering script based on Nebo task requirements:Add OS agnostic

--- scripts/04_rename_with_slice.py
+++ scripts/04_rename_with_slice.py
@@ -6,18 +6,18 @@ if len(sys.argv) != 4:
     print("Incorrect usage, plz provide arguments like in an example: python script_name.py <folder_path> <file_type> <slicing>")
     sys.exit(1)
 
-folder_path = sys.argv[1]
+folder_path = os.path.join(sys.argv[1])
 file_type = sys.argv[2]
 slicing = int(sys.argv[3])
 
 os.chdir(folder_path)
+
 for file in glob.glob(f"*.{file_type}"):
-    file_name = os.path.splitext(file)[0]
-    extension = os.path.split(file)[1]
+    file_name, extension = os.path.splitext(file)
     new_file_name = f"{file_name[:-slicing]}{extension}"
     try:
-      os.rename(file, new_file_name)
+        os.rename(file, new_file_name)
     except OSError as err:
-      print(err)
+        print(err)
     else:
-      print(f"Renamed{file}to}{new_file_name}")
+        print(f"Renamed {file} to {new_file_name}")
